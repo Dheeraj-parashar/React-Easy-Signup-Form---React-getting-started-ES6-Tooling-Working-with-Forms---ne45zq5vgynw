f55024b85bb6dd91e7efc2630d91201f11c45898
Dheeraj-parashar <127424719+Dheeraj-parashar@users.noreply.github.com>
2023-04-19 17:08:20 +0530

    rd

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import '../styles/App.css';
+import { signUpFormValidation } from '../utils/validation';
 
 
 const App = () => {
   
   return (
-
+    <form>
+    <label for="name">Name
+      <input type="text" name="name" id='name' value=""></input>
+    </label><br/><br/>
+   
+    <label for="email">Email
+      <input type="email" name="email" id='email' value=""></input>
+    </label><br/><br/>
+    <label for="password">password
+      <input type="password" name="password" id='password' value=""></input>
+    </label><br/><br/>
+    <label for="consent">Consent
+      <input type="checkbox" name="consent" id='consent' value=""></input>
+    </label><br/><br/>
+    <button id='button' type='submit'>Signup</button>
+    </form>
   )
 }
 
